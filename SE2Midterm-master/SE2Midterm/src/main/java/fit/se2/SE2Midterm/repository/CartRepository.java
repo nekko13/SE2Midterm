0cebfe0067012b0a8c528efdf8eb9b0e78912966
nekko13 <167219927+nekko13@users.noreply.github.com>
2025-03-29 12:57:22 +0700

    Delete SE2Midterm-master directory

--- SE2Midterm-master/SE2Midterm/src/main/java/fit/se2/SE2Midterm/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package fit.se2.SE2Midterm.repository;
-
-import fit.se2.SE2Midterm.model.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CartRepository extends JpaRepository<Cart, Long> {
-}
--- SE2Midterm-master/SE2Midterm/src/main/java/fit/se2/SE2Midterm/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package fit.se2.SE2Midterm.repository;
-
-import fit.se2.SE2Midterm.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
